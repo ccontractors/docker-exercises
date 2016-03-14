d2acbafd1c3dabb990f97fbe448f7b787c01157c
Jesper Wermuth <wermuth@lundogbendsen.dk>
2016-03-14 11:04:41 +0100

    Info exersize better

--- grundlaeggende-container-kommandoer/info/logs-stat-top/solution.sh
+++ grundlaeggende-container-kommandoer/info/logs-stat-top/solution.sh
@@ -7,6 +7,12 @@ docker run --name min-jenkins -d -p 9999:8080 jenkins
 echo Monitorer logs så du hele tiden ser hvad der sker i jenkins
 xterm -hold -e "docker logs -f min-jenkins ; read" &
 
+echo Undersøg hvor meget load er der på containeren
+xterm -hold -e "docker stats min-jenkins ; read" &
+
+echo Hvilke processer kører i containeren ?
+xterm -hold -e "docker top min-jenkins ; read" &
+
 echo Vent lidt og ryd op
 sleep 40
 docker stop min-jenkins
