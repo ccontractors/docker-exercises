c219cdd7f44433373a90a0f497dcefd9ea97a19d
Jesper Wermuth <wermuth@lundogbendsen.dk>
2016-03-14 11:32:51 +0100

    More exersizes

---
+++ interaction-med-containere/run/solution.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+source ~/.profile
+
+echo Udskriv /etc/issue fra et ubuntu image version 14.04
+docker run --rm ubuntu:14.04 cat /etc/issue
+
+echo Udskriv /etc/issue fra et ubuntu image version 15.04
+docker run --rm ubuntu:15.04 cat /etc/issue
+
+echo Udskriv /etc/issue fra et ubuntu image version latest
+docker run --rm ubuntu cat /etc/issue
+
+docker run --rm -ti ubuntu bash
+# Then do less /etc/password
