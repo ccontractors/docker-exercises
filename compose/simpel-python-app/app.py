1d877760c174bedeb6ee78381aa58705acd7d619
Jesper Wermuth <wermuth@lundogbendsen.dk>
2016-03-15 17:16:12 +0100

    python app OK

--- compose/simpel-python-app/app.py
+++ compose/simpel-python-app/app.py
@@ -7,9 +7,8 @@ redis = Redis(host='redis', port=6379)
 
 @app.route('/')
 def hello():
-    redis.incr('hits')
+    rediss.incr('hits')
     return 'Hello World! I have been seen %s times. This one was on machine %s' % (redis.get('hits'), socket.gethostname())
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
